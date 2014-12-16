92aa7e0d85cb912abb2bc096cc2f151833d5be64
bolshakovinka <bolshakovinka@gmail.com>
2014-12-16 22:12:43 +0300

    order1

--- views/pages/order.php
+++ views/pages/order.php
@@ -2,7 +2,7 @@
 <h1 align="center">Оформление заказа</h1><br>
 
 <?
-	if($_SESSION['cart'] && (!isset($_POST['order'])))
+	if($_SESSION['cart'] )
 	{
 ?>
 
@@ -31,17 +31,17 @@
 </table>
     <p class="total zhir" align="center" style="font-size:20; font-family: 'Comic Sans MS', cursive;">Общая сумма заказа: <span style="color: 8B0000;"><?php echo $_SESSION['total_price'] ?> рублей</span> </p>
 	<p align="center" class="zhir">
-	Ваше имя:</br>
+	Ваше имя*:</br>
 	<input type="text" name="first_name"/><br><br>
-	Ваша фамилия:</br>
+	Ваша фамилия*:</br>
 	<input type="text" name="last_name"/><br><br>
 	Ваше отчество:</br>
 	<input type="text" name="patronymic"/><br><br>
-	Ваш телефон:</br>
+	Ваш телефон*:</br>
 	<input type="text" name="phone"/><br><br>
-	Адрес доставки:</br>
+	Адрес доставки*:</br>
 	<input type="text" name="address"/><br><br>
-	Тип оплаты(нал/безнал):</br>
+	Тип оплаты(нал/безнал)*:</br>
 	<input type="text" name="type_pay"/><br><br>
 	</p>
 	
@@ -49,34 +49,58 @@
     <p align="center"><input type="submit" name="order" value="Заказать" /> </p>
 	
 </form>﻿
+<?
 
-<!--<center><p  id="dobav_nadp1" class="zhir"><a href="index.php?view=order">Оформить заказ</a></p></center><br>-->
+//-----------------------------------------------------------------------------------------
+//Проверяем, была ли форма отправлена
+if(isset($_POST['order'])){
+
+   //Создаем массив, в который будем складывать ошибки
+   $errors = array();
+
+   //Проверяем указал ли пользователь имя
+	if(empty($_POST['first_name'])||empty($_POST['last_name'])||empty($_POST['phone'])||empty($_POST['address'])||empty($_POST['type_pay'])){
+      $errors[]=  "*Пожалуйста, заполните все поля, обязательные к заполнению*";
+	}
+	if (!empty($_POST['phone'])){
+			$pattern = "#^( +)?((\+?7|8) ?)?((\(\d{3}\))|(\d{3}))?( )?(\d{3}[\- ]?\d{2}[\- ]?\d{2})( +)?$#"; 
+		if(!preg_match($pattern, $_POST['phone'], $out)){ 
+		$errors[]=  "*Пожалуйста, укажите корректный телефон*";
+		} 
+	}
+	if(!empty($_POST['type_pay']) && !($_POST['type_pay']=='нал' && $_POST['type_pay']=='безнал')){
+      $errors[]=  "*Пожалуйста, в поле <Тип оплаты> укажите либо <нал> либо <безнал>*";
+  }
+}?>
+
+<?php if(isset($errors)){ ?>
+   <ul id="error-list">
+      <?php foreach($errors as $msg){?>
+         <p align="center" class="error zhir"> <?php echo $msg ?> </p>
+    <?  }?>
+   </ul>    
+<?php } ?>
+
+
 <center><img src='images/kart_in_order1.png' height="400"></center>
 
-<?php
+
+<?
 }
+
+/*-----------------------------------------------------------------------------------------
 	if($_SESSION['cart'] && (isset($_POST['order'])))
 	{
-		$first_name=$_POST['first_name'];
-		$last_name=$_POST['last_name'];
-		$patronymic=$_POST['patronymic'];
-		$phone=$_POST['phone'];
 		$address=$_POST['address'];
-		$type_pay=$_POST['type_pay'];
-		$all_price=$_SESSION['total_price'];
-		$pizza_id=$product['pizza_id'];
-		$date_order=date('Y-m-d H:i:s');
-		$query = mysql_query("INSERT INTO orders(first_name,last_name,
-			patronymic,phone,address,type_pay, date_order, 
-			all_price, complited, manager_id) VALUES ('$first_name','$last_name','$patronymic',
-			'$phone','$address','$type_pay','$date_order', '$all_price',0,1)");
-		$id1 = mysql_insert_id();
+		
+		$date=date('Y-m-d');
+		$time=date('H:i:s');
+		
 		foreach($_SESSION['cart'] as $id => $quantity):
-			$product=get_product($id);
-			$pizza_id=$product['pizza_id'];
-			$query2=mysql_query("INSERT INTO orders_has_pizza(orders_id,pizza_id,amount_pizz)
-			VALUES('$id1','$pizza_id','$quantity')");
+		$product=get_product($id);
+			//$query=mysql_query("INSERT INTO order(type_pay,address) VALUES ('$type_pay','$address')");
+			//$query=mysql_query("INSERT INTO address(city) VALUES ('$address')");
 		endforeach;
 		
 	}
-?>
+?>*/?>
